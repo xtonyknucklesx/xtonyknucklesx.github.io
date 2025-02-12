6677b8b8bcb4be2e30be4953cc041a00525dbebe
xtonyknucklesx <xtonyknucklesx@users.noreply.github.com>
2025-02-12 05:47:54 +0000

    deploy: 59dd3dbef98fbf6dacc7749620bcda5f6b7e8af8

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [,{
+    "title": "About",
+    "excerpt":"Test  ","url": "https://xtonyknucklesx.github.io/about/"
+  },{
+    "title": "Tony Rossi",
+    "excerpt":"About Me I’m Tony Rossi, a cybersecurity professional with over 21 years of experience in vulnerability management, threat modeling, GRC, and entrepreneurship. My journey began in the U.S. Navy, where I built the first cybersecurity program for the submarine force and automated critical tasks using Python and open-source tools. Today,...","url": "https://xtonyknucklesx.github.io/"
+  }]
